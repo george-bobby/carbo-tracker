fb1fa66db8ae38a8fe488f906e866a5cab8cb1c2
George Bobby <mailto.georgebobby@gmail.com>
2024-12-24 19:24:41 +0530

    renaming utils

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "../utils"
+import { cn } from "../helpers"
 
 const Table = React.forwardRef(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
