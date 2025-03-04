264f1dd15226cb217eec2b34e30f3f047514dc74
George Bobby <georgebobbyawesome@gmail.com>
2025-03-04 12:34:03 +0530

    fix spelling

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,7 +8,7 @@ import PieChart from './pieChart';
 import EquivalenciesTable from './eqvTable';
 import CarbonComparison from './compareChart';
 import CarbonGauge from './doughnutChart';
-import LineChart from './LineChart';
+import LineChart from './lineChart';
 import RadarChart from './RadarChart';
 import { Tab } from '@headlessui/react';
 import {
