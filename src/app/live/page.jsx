c6c4d0080077caf759119cf960463fcaae768b51
George Bobby <georgebobbyawesome@gmail.com>
2025-03-15 10:58:36 +0530

    search bar

--- src/app/live/page.jsx
+++ src/app/live/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-// import { SearchBar } from './SearchBar';
+import SearchBar from './SearchBar';
 import { CurrentConditions } from './CurrentConditions';
 import { DetailedStats } from './DetailedStats';
 import { WeatherTrends } from './WeatherTrends';
@@ -210,9 +210,9 @@ export default function Live() {
 						</p>
 					</div>
 
-					{/* <div className='max-w-xl mx-auto'>
+					<div className='max-w-xl mx-auto'>
 						<SearchBar />
-					</div> */}
+					</div>
 				</div>
 			</div>
 
