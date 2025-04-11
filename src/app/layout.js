afbe4b02c306aff77ee1f5c07230f7803148743a
George Bobby <georgebobbyawesome@gmail.com>
2025-04-11 17:42:11 +0530

    clarity tracking code

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Script from 'next/script';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -19,6 +20,16 @@ export default function RootLayout({ children }) {
 					<link rel='icon' href='/favicon.ico' />
 				</Head>
 				<body className={inter.className}>
+					{/* Microsoft Clarity Tracking */}
+					<Script id='microsoft-clarity' strategy='afterInteractive'>
+						{`
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "r2gvzbcrod");
+						`}
+					</Script>
 					<Header />
 					<main>{children}</main>
 					<Footer />
